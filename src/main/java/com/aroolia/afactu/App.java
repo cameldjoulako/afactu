1f9fc4d5e2b6c14d04118981b0d4069307ed5ac0
cameldjoulako <cameldjoulako17@gmail.com>
2026-07-11 21:40:56 +0100

    edit config file

--- src/main/java/com/aroolia/afactu/App.java
+++ src/main/java/com/aroolia/afactu/App.java
@@ -2,6 +2,7 @@ package com.aroolia.afactu;
 
 import com.aroolia.afactu.controller.InvoiceControllerInterface;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
@@ -12,7 +13,9 @@ public class App
     public static void main( String[] args )
     {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         InvoiceControllerInterface invoiceController  = context.getBean(InvoiceControllerInterface.class);
 
