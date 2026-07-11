56a7bcf2fb63dfd228f76f6d010865973e48e35e
cameldjoulako <cameldjoulako17@gmail.com>
2026-07-11 22:07:07 +0100

    edit config

--- src/main/java/com/aroolia/afactu/AppConfig.java
+++ src/main/java/com/aroolia/afactu/AppConfig.java
@@ -2,11 +2,13 @@ package com.aroolia.afactu;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = {"com.aroolia.afactu.controller.web", "com.aroolia.afactu.service.prefix", "com.aroolia.afactu.repository.database"})
-@PropertySource("classpath:application.properties")
+//@PropertySource("classpath:application.properties")
+@ImportResource("classpath:applicationContext.xml")
 public class AppConfig {
 
 
