b26946fb0cf06de44df830ee82eb5f939af3f1a7
cameldjoulako <cameldjoulako17@gmail.com>
2026-07-14 22:53:35 +0100

    config

--- src/main/java/com/aroolia/afactu/App.java
+++ src/main/java/com/aroolia/afactu/App.java
@@ -3,11 +3,16 @@ package com.aroolia.afactu;
 import com.aroolia.afactu.controller.InvoiceControllerInterface;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
 
-
+@Configuration
+@ComponentScan(basePackages = {"com.aroolia.afactu.controller.web", "com.aroolia.afactu.service.prefix", "com.aroolia.afactu.repository.database"})
+@PropertySource("classpath:application.properties")
 public class App
 {
     public static void main( String[] args )
@@ -15,7 +20,7 @@ public class App
 
         //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
 
         InvoiceControllerInterface invoiceController  = context.getBean(InvoiceControllerInterface.class);
 
