db8e16718a08c0096669f50e92ba62cb70271a8f
Camel Djoulako <cameldjoulako17@gmail.com>
2026-07-23 18:46:40 +0100

    fix springboot version

--- afactu/src/main/java/com/aroolia/afactu/App.java
+++ afactu/src/main/java/com/aroolia/afactu/App.java
@@ -3,29 +3,30 @@ package com.aroolia.afactu;
 import com.aroolia.afactu.controller.InvoiceControllerInterface;
 import com.aroolia.afactu.service.InvoiceServiceInterface;
 import com.aroolia.afactu.service.prefix.InvoiceServicePrefix;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
+
 @SpringBootApplication
 /*@Configuration
 @ComponentScan
 */
-@PropertySource("classpath:application.properties")
+//@PropertySource("classpath:application.properties")
 public class App
 {
     public static void main( String[] args )
     {
 
         //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml" );
+        //ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class, args);
 
         InvoiceControllerInterface invoiceController  = context.getBean(InvoiceControllerInterface.class);
-
         invoiceController.createInvoice();
-
     }
 }
