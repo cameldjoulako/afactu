fe34479df5af33de87df6fb0a400f42df5443655
Camel Djoulako <cameldjoulako17@gmail.com>
2026-08-05 17:48:38 +0100

    feat: controleurs web/REST, formulaire valide et templates Thymeleaf
    Regroupe le travail accumule sur l'application : InvoiceControllerWeb
    (pages home / details / creation), InvoiceResource (API REST),
    InvoiceForm avec contraintes de validation et messages FR, les
    templates Thymeleaf et les assets statiques CSS/JS.
    
    Ajoute egalement la configuration datasource MySQL et passe le
    context-path a /afactu.

--- afactu-pro/src/main/java/com/aroolia/afactu/AfactuProApplication.java
+++ afactu-pro/src/main/java/com/aroolia/afactu/AfactuProApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AfactuProApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AfactuProApplication.class, args);
+
+        SpringApplication.run(AfactuProApplication.class, args);
 	}
 
 }
--- afactu-pro/src/main/java/com/aroolia/afactu/controller/InvoiceControllerWeb.java
+++ afactu-pro/src/main/java/com/aroolia/afactu/controller/InvoiceControllerWeb.java
@@ -4,18 +4,25 @@ import com.aroolia.afactu.core.controller.InvoiceControllerInterface;
 import com.aroolia.afactu.core.entity.Invoice;
 
 import com.aroolia.afactu.core.service.InvoiceServiceInterface;
+import com.aroolia.afactu.form.InvoiceForm;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+
 import java.util.List;
 
 import static java.lang.System.*;
 
 @Controller
-public class InvoiceControllerWeb implements InvoiceControllerInterface {
+@RequestMapping("/invoice")
+public class InvoiceControllerWeb {
 
     @Autowired
     private InvoiceServiceInterface invoiceService;
@@ -28,24 +35,72 @@ public class InvoiceControllerWeb implements InvoiceControllerInterface {
         this.invoiceService = invoiceService;
     }
 
-    public void createInvoice(){
+    @PostMapping("/create")
+    public String createInvoice(@Valid @ModelAttribute InvoiceForm invoiceForm, BindingResult bindingResult) {
 
-        String customerName="Tesla";
-        Invoice invoice=new Invoice();
-        invoice.setCustomerName(customerName);
+        if (bindingResult.hasErrors()) {
+            return "invoice-create-form";
+        }
+        Invoice invoice = new Invoice();
+        invoice.setCustomerName(invoiceForm.getCustomerName());
+        invoice.setOrderNumber(invoiceForm.getOrderNumber());
 
         invoiceService.createInvoice(invoice);
+
+        return "invoice-created";
     }
 
-    @RequestMapping("/invoice-home")
+    /*@RequestMapping("/home")
     public @ModelAttribute("invoices") List<Invoice> displayHome(HttpServletRequest request) {
-
         out.println("La méthode displayHome a été invoquée") ;
         List<Invoice> invoices = invoiceService.getInvoiceList() ;
-
-        //request.setAttribute("invoices", invoices);
-        //return "index" ;
-
         return invoices;
+    }*/
+
+    /*@RequestMapping("/home")
+    public ModelAndView displayHome() {
+        out.println("La méthode displayHome a été invoquée") ;
+
+        ModelAndView mv = new ModelAndView("invoice-home");
+        mv.addObject("invoices", invoiceService.getInvoiceList());
+
+        return mv;
     }
+
+    @RequestMapping("/{id}")
+    public ModelAndView displayInvoice(@PathVariable("id") String number) {
+
+        out.println("La méthode displayInvoice a été invoquée") ;
+        ModelAndView mv = new ModelAndView("invoice-details");
+        mv.addObject("invoice", invoiceService.getInvoiceByNumber(number));
+
+        //List<Invoice> invoices = invoiceService.getInvoiceList() ;
+
+        return mv;
+    }*/
+
+    @GetMapping("/home")
+    public String displayHome(Model model) {
+        out.println("La méthode displayHome a été invoquée") ;
+        model.addAttribute("invoices", invoiceService.getInvoiceList());
+        return "invoice-home";
+    }
+
+    /*@GetMapping("/{id}")
+    public String displayInvoice(@PathVariable("id") String number, Model model) {
+
+        out.println("La méthode displayInvoice a été invoquée") ;
+        model.addAttribute("invoice", invoiceService.getInvoiceByNumber(number));
+        //List<Invoice> invoices = invoiceService.getInvoiceList() ;
+
+        return "invoice-details";
+    }*/
+
+    @GetMapping("/create-form")
+    public String displayInvoiceCreateForm(@ModelAttribute InvoiceForm invoice) {
+        return "invoice-create-form";
+    }
+
+
+
 }
