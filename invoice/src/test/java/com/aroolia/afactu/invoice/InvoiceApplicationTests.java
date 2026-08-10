6c193c8b3b5eee7d82de346129eb06d110580fa5
Camel Djoulako <cameldjoulako17@gmail.com>
2026-08-10 22:24:52 +0100

    init invoice microservice

---
+++ invoice/src/test/java/com/aroolia/afactu/invoice/InvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aroolia.afactu.invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
